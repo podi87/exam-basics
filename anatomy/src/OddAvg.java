3443194398f31a0b7ae46f8615636bef2156fd99
Andras Podobni <podobni.andras@gmail.com>
2017-04-18 14:02:24 +0200

    add copied files to Temp folder.

---
+++ anatomy/src/OddAvg.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddAvg {
+  public static void main(String[] args) {
+    // Create a function called `oddAverage` that takes a list of numbers as parameter
+    // and returns the average value of the odd numbers in the list
+    // Create basic unit tests for it with at least 3 different test cases
+    List<Integer> numberList = new ArrayList<>();
+    for (int i = 1; i < 6; i++) {
+      numberList.add(i);
+    }
+
+    public int oddAverage(List numberList) {
+    for (int i = 0; i < numberList.size(); i++) {
+      if (numberList.i)
+    }
+  }
+}
